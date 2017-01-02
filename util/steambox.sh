1e001e9f908b1927dbf652832d3af86507ab0237
rsharo <rsharo@users.noreply.github.com>
2017-01-02 18:26:31 -0500

    Steam launcher now launches in steambox.

--- util/steambox.sh
+++ util/steambox.sh
@@ -2,40 +2,51 @@
 
 # Launches the steambox image with direct connections to the specified
 # user's X session, ALSA, and PulseAudio.
+#
+# Author: rsharo <rsharo@users.noreply.github.com>
 
-if [[ -n "${STEAMUSER_UID}" ]] ; then
-	echo "$0: you must set the environment variable STEAMUSER_UID"
-fi
+### functions
 
-if [[ -n "${STEAMUSER_HOME}" ]] ; then
-	echo "$0: you must set the environment variable STEAMUSER_HOME"
-fi
-
-cat << EOF
-WARNING: This script launches a steambox Docker container with
+function disclaimer() {
+	cat << EOF
+$0: WARNING: This script launches a steambox Docker container with
 unrestricted access to your X session and audio drivers.
 
 There are no provisions stopping the container from:
    1) Showing you spoofed windows, including prompts to enter your password.
    2) Accessing your microphone
    3) Capturing mouse gestures and/or keystrokes
-   4) Accessing other microphones on your network if you are tunneling PulseAudio
+   4) Playing polka music or even worse... Justin Bieber.
 
 EOF
 
-read -n 1 -t 20 -p "Are you sure you want to continue? [N/y] " response
-echo
-if [[ "${response}" != "y" ]]; then
-	echo "Exiting." >&2
-	exit 1
-fi
+	read -n 1 -t 20 -p "Are you sure you want to continue? [N/y] " response
+	echo
+	if [[ "${response}" != "y" ]]; then
+		echo "Exiting." >&2
+		exit 1
+	fi
+}
 
+
+### Main script
+
+disclaimer $0
+
+STEAMUSER_DISPLAY=${STEAMUSER_DISPLAY:-":0"}
+
+echo STEAMUSER_UID=${STEAMUSER_UID:?"$0: you must set the environment variable STEAMUSER_UID"}
+echo STEAMUSER_HOME=${STEAMUSER_UID:?"$0: you must set the environment variable STEAMUSER_HOME"}
+echo STEAMUSER_DISPLAY=${STEAMUSER_DISPLAY}
 
 STEAMHOME="${STEAMUSER_HOME}/steamhome"
 
 declare -a HOMEDIR_ARGS=( -v "${STEAMHOME}:/home/steamuser" )
 
-declare -a X11_ARGS=( -v /tmp/.X11-unix:/tmp/.X11-unix )
+declare -a X11_ARGS=(
+	-v /tmp/.X11-unix:/tmp/.X11-unix
+	--env "DISPLAY=${STEAMUSER_DISPLAY}"
+)
 
 declare -a ALSA_ARGS=( --device /dev/snd )
 
@@ -58,4 +69,7 @@ fi
 docker run -ti --rm --name steambox \
 	"${HOMEDIR_ARGS[@]}" "${X11_ARGS[@]}" "${ALSA_ARGS[@]}" "${PULSE_ARGS[@]}" \
 	steambox "$@"
+
+
+### functions
 
